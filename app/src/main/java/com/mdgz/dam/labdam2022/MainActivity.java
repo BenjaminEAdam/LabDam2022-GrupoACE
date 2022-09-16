b8a956cc074036cd3d2fa2914edc3f4d08b27a67
benja <benjaadam14@gmail.com>
2022-09-16 20:43:18 -0300

    Primer commit - Inciso 1

--- app/src/main/java/com/mdgz/dam/labdam2022/MainActivity.java
+++ app/src/main/java/com/mdgz/dam/labdam2022/MainActivity.java
@@ -3,12 +3,25 @@ package com.mdgz.dam.labdam2022;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
+
+    Menu menu;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.mi_menu_toolbar, menu);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setSupportActionBar(findViewById(R.id.materialToolbar));
+
+
     }
 }
